3841dcd615d29560cdff1ea688901450198a3199
Erastus Ruiru <erastusruiru11@gmail.com>
2020-10-20 21:24:58 +0300

    add dotenv

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: process.env.REACT_APP_API,
 });
 
 ReactDOM.render(
