1aa4f1419fe79b6525a906f4980703c074058505
Erastus Ruiru <erastusruiru11@gmail.com>
2020-10-21 00:30:42 +0300

    add an about section

--- src/Components/Categories/categories.tsx
+++ src/Components/Categories/categories.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Joke from "../Joke/index";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import { Jumbotron } from "reactstrap";
+
 import {
   CardContent,
   CardFooter,
@@ -28,14 +30,36 @@ const Example = () => {
     setCategory("");
   };
 
-  let { data} = useQuery(getAllCategories);
+  let { data } = useQuery(getAllCategories);
+
+  console.log();
   return (
     <>
-      {isOpen ? <Joke onClose={onClose} category={category} isOpen={isOpen} /> : null}
+      {isOpen ? (
+        <Joke onClose={onClose} category={category} isOpen={isOpen} />
+      ) : null}
       <Container>
+        <Jumbotron>
+          <h1 className="display-3">Chucknorris Jokes</h1>
+          <p className="lead">
+            This is an SAP application that shows of some simple yet powerful
+            concepts.
+          </p>
+          <p>{"✅"}{""} Styles components</p>
+          <p>{"✅"} {""}Apollo Client</p>
+          <p>{"✅"} {""}TypeScript</p>
+          <p>{"✅"}{""} Graphql</p>
+
+
+          <hr className="my-2" />
+          <p>
+            It uses utility classes for typography and spacing to space content
+            out within the larger container.
+          </p>
+        </Jumbotron>
         <CardList>
           {data &&
-            data.Categories.map((chat: any,i:number) => (
+            data.Categories.map((chat: any, i: number) => (
               <CardItem key={i}>
                 <CardImage>
                   <img
